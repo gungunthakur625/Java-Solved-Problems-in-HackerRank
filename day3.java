da72ba26798c0d9efeb6ac43e6858c573041db81
gungunthakur625 <gunnuthakur625@gmail.com>
2026-01-02 13:27:10 +0530

    Update day3.java

--- day3.java
+++ day3.java
@@ -1,11 +1,28 @@
-public class While {
-    public static void main(String[] args)
-    {
-        int i=0;
-        while (i<=10) {
-            System.out.println("count "+ i);
-            i++;
+import java.io.*;
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2!=0){
+            System.out.println("Weird");
+        }
+        else{
+            if(N>=2&&N<=5){
+                System.out.println("Not Weird");
+            }
+            else if(N>=6&&N<=20){
+                System.out.println("Weird");
+            }
+            else{
+                System.out.println("Not Weird");
+            } 
         }
 
+        scanner.close();
     }
 }
