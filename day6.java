543eef71dbbb7b84e9d13ce5ee85855fcae71a64
gungunthakur625 <gunnuthakur625@gmail.com>
2026-01-06 14:01:15 +0530

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int I=1; I<=10; I++){
+            
+            System.out.println(N+" x "+I+" = "+(N*I));
+        }
+
+        bufferedReader.close();
+    }
